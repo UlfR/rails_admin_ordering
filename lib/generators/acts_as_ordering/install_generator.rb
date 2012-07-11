f0f818a25472be37c5730a28c250fdce8dca17fe
Ivan Grunev <ivan.grunev@netcook.org>
2012-07-12 00:48:55 +0700

    add functional

--- lib/generators/acts_as_ordering/install_generator.rb
+++ lib/generators/acts_as_ordering/install_generator.rb
@@ -1,4 +1,4 @@
-class ActsAsOrdering::InstallGenerator < Rails::Generators::Base #:nodoc:
+class RailsAdminOrdering::InstallGenerator < Rails::Generators::Base #:nodoc:
   include Rails::Generators::Migration
   source_root File.expand_path('../templates', __FILE__)
   require 'rails/generators/migration'
